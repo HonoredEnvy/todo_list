4974ccdcfc8ddafff527e76e350a4079b1ef348d
= <=>
2025-10-02 08:44:31 +0545

    initial commit

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import './assets/styles/app.css'
+  let note = $state('')
+  let notes = $state([])
+</script>
+
+<div class="topbar">
+  <h1 class="label">celeron</h1>
+  <div class="minilabel">simple todo list app</div>
+</div>
+<input bind:value={note} type="text" />
+<button
+  onclick={() => {
+    if (note.trim() === '') return
+    notes.push(note)
+    note = ''
+  }}>New</button
+>
+<div>
+  {#each notes as n, i (i)}
+    <span>{n}</span>
+    <button
+      onclick={() => {
+        notes.splice(i, 1)
+      }}>delete</button
+    > <br />
+  {/each}
+</div>
